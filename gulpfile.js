2e76dfc07dd4a9d405135cf8c6c89f6ba5b99109
nitroclik <reimon1972@gmail.com>
2016-10-22 09:25:24 +0200

    insert matarialize

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 'use strict';
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+
+gulp.task('sass', function() {
+    return gulp.src('./app/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'));
 });
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+
+gulp.task('sass:watch', function() {
+    gulp.watch('./app/sass/**/*.scss', ['sass']);
 });
+
+gulp.task('default', ['sass:watch']);
